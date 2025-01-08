ef3e994a6e34324e9ebe44804b7d19fecb2530e8
KHminor <ghdals2020@gmail.com>
2025-01-08 18:00:05 +0900

    nextJS

---
+++ start/src/components/layout/nav.jsx
@@ -0,0 +1,9 @@
+export default function Nav() {
+    return (
+        <>
+            <nav className="fixed h-3 w-full bg-cyan-400">
+                
+            </nav>
+        </>
+    )
+}
---
+++ start/src/pages/cart/index.jsx
@@ -0,0 +1,8 @@
+import Nav from "@/components/layout/nav";
+
+export default function Cart() {
+    return <>
+        <Nav/>
+        <div>Cart속 Index</div>
+    </>
+}
